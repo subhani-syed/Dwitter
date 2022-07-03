1e83baebdb6c9250936129ae3b30c7bd7f17452d
Subhani-syed <subhani6352@gmail.com>
2022-07-03 23:52:29 +0530

    Created Models

--- Home/admin.py
+++ Home/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import Profile,Dweet,UserFollowing
 # Register your models here.
+
+admin.site.register(Profile)
+admin.site.register(Dweet)
+admin.site.register(UserFollowing)
--- Home/models.py
+++ Home/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Profile(models.Model):
+    bio = models.CharField(max_length=100,null=True)
+    followers = models.PositiveIntegerField(null=True,default=0)
+    following = models.PositiveIntegerField(null=True,default=0)
+    user = models.OneToOneField(User,on_delete=models.CASCADE)
+    def __str__(self):
+        return f'{self.user.username} Profile'
+
+    
+class Dweet(models.Model):
+    dweet = models.CharField(max_length=100)
+    user_id = models.ForeignKey(User,related_name="Author",on_delete=models.CASCADE)
+    time = models.DateTimeField()
+
+    def __str__(self):
+        return f'{self.user_id}"s Dweet'
+
+
+class UserFollowing(models.Model):
+    # Need to switch following followers names
+    user_id = models.ForeignKey(User, related_name="following",on_delete=models.CASCADE)
+    following_user_id = models.ForeignKey(User, related_name="followers",on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.user_id}"s relation'
